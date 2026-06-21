989ca8ec70bd2eb080073a61f1a32103cfc2f41d
TufailMd <jrtufailmd@gmail.com>
2026-06-21 17:14:51 +0530

    chore: add repo

--- server/controllers/add.js
+++ server/controllers/add.js
@@ -1,3 +1,15 @@
-export async function addRepo() {
-  console.log("add cmd called");
+import fs from "fs/promises";
+import path from "path";
+
+export async function addRepo(filePath) {
+  const repoPath = path.resolve(process.cwd(), ".kod");
+  const stagingPath = path.join(repoPath, "staging");
+  const fileName = path.basename(filePath);
+  try {
+    await fs.mkdir(stagingPath, { recursive: true });
+    await fs.copyFile(filePath, path.join(stagingPath, fileName));
+    console.log(`File ${fileName} is added to stage area!`);
+  } catch (error) {
+    console.log(`Error adding file: \n ${error}`);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -18,7 +18,9 @@ yargs(hideBin(process.argv))
         type: "string",
       });
     },
-    addRepo,
+    (argv) => {
+      addRepo(argv.file);
+    },
   )
   .command(
     "commit <message>",
