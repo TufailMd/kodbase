420b29c86aed94680e277d6f9ca695ff76001a39
TufailMd <jrtufailmd@gmail.com>
2026-06-23 03:55:54 +0530

    feat: implement pushRepo S3 upload

--- server/config/aws-config.js
+++ server/config/aws-config.js
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import AWS from "aws-sdk";
 
-AWS.config.update({ region: "ap-south-1" });
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
-const s3 = new AWS.S3();
-const S3_BUCKET = "kodbasetestbucket";
+const S3_BUCKET = process.env.S3_BUCKET_NAME;
 
 export { s3, S3_BUCKET };
--- server/controllers/push.js
+++ server/controllers/push.js
@@ -1,3 +1,35 @@
-export function pushRepo() {
-  console.log("push cmd called");
+import fs from "fs/promises";
+import path from "path";
+import { s3, S3_BUCKET } from "../config/aws-config.js";
+
+export async function pushRepo() {
+  const repoPath = path.resolve(process.cwd(), ".kod");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const commitDirs = await fs.readdir(commitsPath);
+
+    for (const commitDir of commitDirs) {
+      const commitPath = path.join(commitsPath, commitDir);
+      const files = await fs.readdir(commitPath);
+
+      for (const file of files) {
+        const filePath = path.join(commitPath, file);
+        const fileContents = await fs.readFile(filePath);
+
+        const params = {
+          Bucket: S3_BUCKET,
+          Key: `commits/${commitDir}/${file}`,
+          Body: fileContents,
+        };
+
+        await s3.upload(params).promise();
+      }
+
+      console.log("All commit pushed to S3");
+      
+    }
+  } catch (error) {
+    console.log(`Error pushing to S3 : ${error}`);
+  }
 }
